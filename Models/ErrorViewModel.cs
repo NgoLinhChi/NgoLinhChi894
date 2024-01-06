6c42355e6f8261fd4f75246f8dd521fe322af0a6
NgoLinhChi <lhcii242@gmail.com>
2024-01-06 10:43:14 +0700

    NgoLinhChi894

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NgoLinhChi894.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
